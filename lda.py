b50de76c20bcf677c3876dcae3c260c17fae6401
Tracywangsw <wangsheng1025@gmail.com>
2016-05-21 15:38:45 +0800

    calculate perplex

--- lda.py
+++ lda.py
@@ -1,8 +1,9 @@
 import pynlpir
-#from gensim import corpora, models
-#import gensim
+from gensim import corpora, models
+import gensim
 import os
-import shutil
+import io
+import csv
 
 class LDA(object):
   """docstring for LDA"""
@@ -13,25 +14,20 @@ class LDA(object):
   def _part_document(self):
     pynlpir.open()
     docs = []
-    for dirname, dirnames,filenames in os.walk('./data'):
-      i = 0
+    for dirname, dirnames,filenames in os.walk('dependence/data'):
       for filename in filenames:
-        i += 1
-        if i>4944: break
         path = os.path.join(dirname, filename)
         text = ''
-        with open(path) as f:
-          print(path)
+        with io.open(path, 'r',encoding='utf-8') as f:
           text = f.readline()
           words = pynlpir.segment(text,pos_tagging=False)
-          clean_words = [w for w in words if w not in self.stop_list]
+          clean_words = [w for w in words if w not in self.stop_list and len(w)>1]
           docs.append(clean_words)
-          print(len(docs))
     return docs
 
-  def _get_stop_list(self,path='new_list.txt'):
+  def _get_stop_list(self,path='dependence/new_list.txt'):
     stop_list = []
-    with open(path,encoding="utf-8") as f:
+    with io.open(path,'r',encoding='utf-8') as f:
       lines = f.readlines()
       for l in lines:
         l = l.strip()
@@ -40,10 +36,10 @@ class LDA(object):
 
   def train(self,num_topics):
     dictionary = corpora.Dictionary(self.docs)
-    corpora = [dictionary.doc2bow(text) for text in self.docs]
-    # model = gensim.models.ldamodel.LdaModel(self.corpora, num_topics=num_topics, id2word=dictionary, alpha='auto', minimum_probability=0)
-    gensim.models.wrappers.LdaMallet('mallet-2.0.8RC3/bin/mallet', corpus=corpora, num_topics=num_topics, id2word=dictionary, workers=4, prefix=None, optimize_interval=10, iterations=2000)
-    perplex = model.bound(corpora)
+    corporas = [dictionary.doc2bow(text) for text in self.docs]
+    model = gensim.models.ldamodel.LdaModel(corporas, num_topics=num_topics, id2word=dictionary, minimum_probability=0, iterations=2000)
+    # model = gensim.models.wrappers.LdaMallet('mallet-2.0.8RC3/bin/mallet', corpus=corporas, num_topics=num_topics, id2word=dictionary, workers=4, prefix=None, optimize_interval=10, iterations=2000)
+    perplex = model.bound(corporas)
     return perplex
 
 
@@ -61,12 +57,9 @@ def make_stop_list():
     for s in stop_list:
       f.write(s+'\n')
 
-def _part_document():
+def part_document():
   for dirname, dirnames,filenames in os.walk('./data'):
-    i = 0
     for filename in filenames:
-      i += 1
-      if i>4944: break
       path = os.path.join(dirname, filename)
       text = ''
       with open(path) as f:
@@ -76,12 +69,33 @@ def _part_document():
       with open(write_path,'w',encoding='utf-8') as f:
         f.write(text)
 
-def move_file():
-  for dirname, dirnames,filenames in os.walk('./data'):
-    i = 0
-    for filename in filenames:
-      i += 1
-      if i>4944: break
-      if filename.endswith('_1.txt'):
-        path = os.path.join(dirname, filename)
-        shutil.move(path,"./new_data/"+filename)
+def optimize_parameters(filename):
+  num_topic_list = [10*x for x in range(1,20)]
+  m = LDA()
+  perplex_list = []
+  for n in num_topic_list:
+    perplex = m.train(n)
+    perplex_list.append(perplex)
+    print([n,perplex])
+  with open('dependence/perplex/'+filename,'w',newline='') as f:
+    a = csv.writer(f)
+    for s in zip(num_topic_list,perplex_list):
+      a.writerow(s)
+  return max(perplex_list)
+
+class CompanySimilairy(object):
+  """docstring for CompanySimilairy"""
+  def __init__(self):
+    self.company_list = self._get_company_list()
+
+  # def _get_company_list():
+
+    
+  def topic_similarity(self,topic_a,topic_b):
+    kl = self.kl_divergence(topic_a,topic_b)
+    return math.exp(-1*kl)
+
+  def kl_divergence(self,p,q):
+    return np.sum([stats.entropy(p,q),stats.entropy(q,p)])
+
+  # def save_document_similarity_matrix():
